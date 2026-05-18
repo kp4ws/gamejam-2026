77d703d42444dd825c8bae12fc6fee1fb2ad7c85
kp4ws <kentpawson123@gmail.com>
2026-05-18 12:57:13 -0700

    Added MainMenuScene

--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -1,11 +1,53 @@
 import Phaser from "phaser";
 
 export default class MainMenuScene extends Phaser.Scene {
-    constructor() {
-        super("MainMenu");
-    }
+  constructor() {
+    super("MainMenu");
+  }
 
-    create() {
+  create() {
+    const { width, height } = this.scale;
 
-    }
+    //TODO init audio
+
+    //Title
+    this.add
+      .text(width / 2, height / 2 - 40, "MY GAME", {
+        fontFamily: "monospace",
+        fontSize: "32px",
+        color: "#ffffff",
+      })
+      .setOrigin(0.5);
+
+    //Start Button
+    const startButton = this.add
+      .text(width / 2, height / 2 + 20, "START", {
+        fontFamily: "monospace",
+        fontSize: "16px",
+        color: "#00ffcc",
+      })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true });
+
+    startButton.on("pointerdown", () => {
+      this.scene.start("Game");
+    });
+
+    startButton.on("pointerover", () => {
+      startButton.setStyle({ color: "#ffffff" });
+    });
+
+    startButton.on("pointerout", () => {
+      startButton.setStyle({ color: "#00ffcc" });
+    });
+
+    //Start button blink effect
+    this.tweens.add({
+        targets: startButton,
+        alpha: 0.5,
+        duration: 600,
+        yoyo: true,
+        repeat: -1,
+    });
+  }
 }
